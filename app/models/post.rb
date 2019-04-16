e4a111bc4e07800227308b5d8370c1f3dd499f06
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-16 00:08:14 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,14 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  
+  validates :clickbait, confirmation: true
+
+  def clickbait
+   bait = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+   if title
+     if bait.none? {|word| title.include?(word)}
+       errors[:title] << "Must be clickbait"
+     end
+   end
+ end
 end
